cbbbcdd786d202079e72ecd5db3831ac5f105b47
Amogh <amoghkrrish1068@gmail.com>
2024-01-16 22:52:39 +0530

    hi

--- src/home.jsx
+++ src/home.jsx
@@ -6,25 +6,25 @@ function Home() {
     <div className="container">
       <div className="left-panel">
         <div className="greeting">
-          <h2>Hi Samantha</h2>
-          <p>Welcome back to the workspace, we missed You!</p>
+          <h2>Hi ALEX</h2>
+          <p>Welcome back to the world, we never missed You!</p>
         </div>
         <div className="projects">
           <h3>Projects (3)</h3>
           <div className="project-item" style={{backgroundColor: "#7FFF00"}}>
-            <p>Green House</p>
+            <p>pubg</p>
           </div>
           <div className="project-item" style={{backgroundColor: "#800080"}}>
-            <p>Cyber Punk</p>
+            <p>valorant</p>
           </div>
           <div className="project-item" style={{backgroundColor: "#FFC0CB"}}>
-            <p>Easy Crypto</p>
+            <p>cod</p>
           </div>
           
         </div>
       </div>
       <div className="right-panel">
-        <h3>Cyber Punk</h3>
+        <h3>CALL OF DUTY</h3>
         <ul className="task-list">
           <li><input type="checkbox" /><span>Create initial layout for homepage</span></li>
           <li><input type="checkbox" /><span>Fixing logo with transparent background</span></li>
